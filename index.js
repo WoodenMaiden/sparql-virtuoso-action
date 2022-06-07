b56a554c5bff610eaae73d742267684b8183cea2
WoodenMaiden <yann.pomie@etu.umontpellier.fr>
2022-06-07 11:50:08 +0200

    debug
    Signed-off-by: WoodenMaiden <yann.pomie@etu.umontpellier.fr>

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const core = require('@actions/core');
-const { exec } = require('child_process');
+const { exec, execSync } = require('child_process');
 
 try {
     const dba_password = core.getInput('dba_password');
     const dav_password = core.getInput('dav_password');
     const triples = core.getInput('triples');
+
+    core.setOutput(execSync('ls'))
+    core.setOutput(execSync('pwd'))
 
     exec(`./entrypoint.sh ${dba_password} ${dav_password} ${triples}`, (error, stdout, stderr) => {
         if (error) throw [ error, stdout, stderr ];
