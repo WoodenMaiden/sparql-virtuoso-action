df7c34fa87f338b279464e021b933e9657cfc5ec
WoodenMaiden <yann.pomie@etu.umontpellier.fr>
2022-06-07 11:15:56 +0200

    fix: wrapped action in a js script
    Signed-off-by: WoodenMaiden <yann.pomie@etu.umontpellier.fr>

---
+++ index.js
@@ -0,0 +1,17 @@
+const core = require('@actions/core');
+const { exec } = require('child_process');
+
+try {
+    const dba_password = core.getInput('dba_password');
+    const dav_password = core.getInput('dav_password');
+    const triples = core.getInput('triples');
+
+    exec(`./entrypoint.sh ${dba_password} ${dav_password} ${triples}`, (error, stdout, stderr) => {
+        if (error) throw error;
+
+        core.setOutput(stdout)
+    })
+
+} catch (error) {
+    core.setFailed(`exec error: ${error}`);
+}
