5f1eff04780823615ecdeda0cbcdd5b3215f40d5
Yann POMIE <yann.pomie@laposte.net>
2022-06-08 17:30:48 +0200

    refacto(WIP): started refactoring

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
+const fs = require('fs');
+
 const core = require('@actions/core');
-const { exec, execSync } = require('child_process');
+const Dockerode = require("dockerode");
 
-try {
-    const dba_password = core.getInput('dba_password');
-    const dav_password = core.getInput('dav_password');
-    const triples = core.getInput('triples');
 
-    core.setOutput(execSync('ls'))
-    core.setOutput(execSync('pwd'))
+async function bootstrap() {
+    try {
+        console.log("start")
+        const dba_password = core.getInput('dba_password');
+        const dav_password = core.getInput('dav_password');
+        const triples = core.getInput('triples');
+    
+        if (!fs.existsSync('./virtuoso.ini')) throw './virtuoso.ini is missing';
+    
+        const docker = new Dockerode()
+        await docker.pruneImages()
+        await docker.pruneContainers()
+        console.log("pruned")
 
-    exec(`./entrypoint.sh ${dba_password} ${dav_password} ${triples}`, (error, stdout, stderr) => {
-        if (error) throw [ error, stdout, stderr ];
+        await docker.buildImage({
+            context: __dirname,
+            src: ['Dockerfile', 'virtuoso.ini']
+          }, {
+            t: "database"
+        })
+        console.log("built")
+        const container = await docker.createContainer({
+            name: "virtuoso", 
+            Env: [`DBA_PASSWORD=${dba_password}`, `DAV_PASSWORD=${dba_password}`],
+            Image: "database",
+            Cmd: ['/opt/virtuoso-opensource/bin/virtuoso-t', '-f', '+checkpoint-only', '+pwdold', 'dba', '+pwddba', dba_password ],
+            Tty: false
+        })
+        const commit = await container.commit()
+        console.log(commit)
+        console.log(typeof commit)
 
-        core.setOutput(stdout)
-    })
-
-} catch (error) {
-    core.setFailed(`error: ${error}\nstdout: ${stdout}\nstderr: ${stderr}`);
+    
+    
+    } catch (error) {
+        core.setFailed(error);
+    }
 }
+bootstrap()
