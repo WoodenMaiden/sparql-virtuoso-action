54be9da7824a02ea74bb0e790cc5ff2e7041644e
Yann POMIE <yann.pomie@laposte.net>
2022-06-10 11:04:04 +0200

    feat: use fillers
    Signed-off-by: Yann POMIE <yann.pomie@laposte.net>

--- index.js
+++ index.js
@@ -1,42 +1,65 @@
 const fs = require('fs');
+const fsp = require('fs/promises');
 
 const core = require('@actions/core');
 const Dockerode = require("dockerode");
 
+const Filler = require('./fillData');
+
 
 async function bootstrap() {
     try {
-        console.log("start")
         const dba_password = core.getInput('dba_password');
         const dav_password = core.getInput('dav_password');
-        const triples = core.getInput('triples');
+        const triplesInput = core.getInput('triples');
     
         if (!fs.existsSync('./virtuoso.ini')) throw './virtuoso.ini is missing';
     
         const docker = new Dockerode()
+        
+        try { // let's try to stop the container just in case...
+            await docker.getContainer("virtuosoaction").stop()
+        } catch (e) {
+            console.log("No container")
+        }
+        
         await docker.pruneImages()
         await docker.pruneContainers()
-        console.log("pruned")
-
+        
         await docker.buildImage({
             context: __dirname,
             src: ['Dockerfile', 'virtuoso.ini']
           }, {
             t: "database"
         })
-        console.log("built")
+        
         const container = await docker.createContainer({
-            name: "virtuoso", 
+            name: "virtuosoaction", 
             Env: [`DBA_PASSWORD=${dba_password}`, `DAV_PASSWORD=${dba_password}`],
             Image: "database",
-            Cmd: ['/opt/virtuoso-opensource/bin/virtuoso-t', '-f', '+checkpoint-only', '+pwdold', 'dba', '+pwddba', dba_password ],
+            Entrypoint: ['/virtuoso-entrypoint.sh'],
+            Cmd: ['start'],
             Tty: false
         })
-        const commit = await container.commit()
-        console.log(commit)
-        console.log(typeof commit)
+        await container.start()
 
-    
+        if (triplesInput && triplesInput.length !== 0) {
+            const files = triplesInput.split(' ');
+            const fillers = []
+
+            for(let i = 0; i< files.length; ++i) {
+                try {
+                    const fileHandler = await fsp.open(files[i], 'r')
+                    fillers.push(new Filler(fileHandler).sendData(container))
+                } catch(e) {
+                    console.log(e)
+                }
+            }
+
+            const res = await Promise.all(fillers)
+          
+        }
+        else console.log('No files provided')
     
     } catch (error) {
         core.setFailed(error);
