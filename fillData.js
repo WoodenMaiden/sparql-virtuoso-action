0912c2228a186bdf4fa44d92cab7bdc011da3266
Yann POMIE <yann.pomie@etu.umontpellier.fr>
2022-07-27 16:58:22 +0200

    Refactonode (#1)
    * refacto(WIP): started refactoring
    
    * feat: add strategy pattern
    
    * feat: use fillers
    
    Signed-off-by: Yann POMIE <yann.pomie@laposte.net>
    
    * feat: use strict
    
    Signed-off-by: Yann POMIE <yann.pomie@laposte.net>
    
    * feat: generate query for csv files
    
    Signed-off-by: Yann POMIE <yann.pomie@laposte.net>
    
    * fix: wait for server to completely start
    
    Signed-off-by: WoodenMaiden <yann.pomie@etu.umontpellier.fr>
    
    * fix: destroy useless streams that made the script go idle
    
    Signed-off-by: Yann POMIE <yann.pomie@laposte.net>
    
    * fix: delete useless files, completed action metadata and handle fails on file opening
    
    Signed-off-by: Yann POMIE <yann.pomie@laposte.net>
    
    * feat: support turtle format and fix error catching
    
    Signed-off-by: Yann POMIE <yann.pomie@laposte.net>

---
+++ fillData.js
@@ -0,0 +1,161 @@
+const uuidv4 = require("crypto").randomUUID
+
+const CSVParse = require('csv-parse').parse;
+const RDFParser = require('rdf-parse').default
+
+class QueryGeneratorStrategy {
+    static REGEX = /(?<protocol>(?:[^:]+)s?)?:\/\/(?:(?<user>[^:\n\r]+):(?<pass>[^@\n\r]+)@)?(?<host>(?:www\.)?(?:[^:\/\n\r]+))(?::(?<port>\d+))?\/?(?<request>[^?#\n\r]+)?\??(?<query>[^#\n\r]*)?\#?(?<anchor>[^\n\r]*)?/m
+
+    constructor() { if (this.constructor === QueryGeneratorStrategy || this.constructor === QueryGeneratorStrategy) throw new Error('This is an abstract class')}
+    execute() {throw new Error('This is an abstract class')}
+}
+
+///////////////////////////////////////////////////////
+
+class CSVQueryGenerator extends QueryGeneratorStrategy{
+    constructor(fileHandler){ 
+        super()
+        this.fileHandler = fileHandler
+    }
+
+    async execute() {
+        return new Promise((res, rej) => {
+            const buffer = [ 'SPARQL INSERT DATA IN GRAPH <http://localhost/data> {' ]
+            this.fileHandler.createReadStream()
+                .pipe(CSVParse({
+                delimiter: ', ',
+                encoding: 'utf-8' 
+            }))
+            .on('data', (data) => {  
+                let toPush = `<${data[0]}> <${data[1]}> `
+                if (data[2].match(QueryGeneratorStrategy.REGEX)) toPush += `<${data[2]}>`
+                else toPush += (data[2].indexOf(`'`) === -1)? `'${data[2]}'`: `"${data[2]}"`
+                toPush += '.\n'
+                
+                buffer.push(toPush)
+            })
+            .on('error', (error) => {
+                rej(error)
+            })
+            .on('end', () => {
+                res(buffer.join('') + "};\n")
+            })
+        })
+    }    
+}
+
+class TurtleQueryGenerator extends QueryGeneratorStrategy{
+    constructor(fileHandler){ 
+        super()
+        this.fileHandler = fileHandler
+    }
+
+    async execute() {
+        return new Promise((res, rej) => {
+            let buffer = ""
+            RDFParser.parse(this.fileHandler.createReadStream(), {contentType: 'text/turtle'})
+            .on('data', (quad) => buffer += `SPARQL INSERT DATA IN GRAPH \
+${(quad._graph.id === '')? '<http://localhost/data>': `<${quad._graph.id}>`} {\
+<${quad._subject.id}> <${quad._predicate.id}> \
+${(quad._object.id.startsWith('"') && quad._object.id.startsWith('"'))? quad._object.id : `<${quad._object.id}>`}.\
+};\n`)
+            .on('error', (err) => rej(err))
+            .on('end', () => res(buffer + "\n"))
+        })
+    }    
+}
+
+
+class RDFXMLQueryGenerator extends QueryGeneratorStrategy{
+    constructor(fileHandler){ 
+        throw new Error('Not yet implemented')
+        super()
+        this.fileHandler = fileHandler
+    }
+
+    async execute() {
+        throw new Error('Not yet implemented')        
+
+        return 'someQuerytoInject'
+    }    
+}
+
+class JSONLDQueryGenerator extends QueryGeneratorStrategy{
+    constructor(fileHandler){ 
+        throw new Error('Not yet implemented')
+        super()
+        this.fileHandler = fileHandler
+    }
+
+    async execute() {
+        throw new Error('Not yet implemented')
+        
+        return 'someQuerytoInject'
+    }    
+}
+
+
+///////////////////////////////////////////////////////
+
+module.exports = class Fill {
+    static fileExtentions = new Map ([
+        [".csv", CSVQueryGenerator],
+        
+        [".ttl", TurtleQueryGenerator],
+        [".turtle", TurtleQueryGenerator],
+
+        [".rdf", RDFXMLQueryGenerator],
+        [".rdfxml", RDFXMLQueryGenerator],
+        [".owl", RDFXMLQueryGenerator],
+        
+        [".jsonld", JSONLDQueryGenerator],
+        [".json", JSONLDQueryGenerator]
+    ])
+
+
+    constructor (filePath, fileHandler) {
+        const gen = Fill.fileExtentions.get(
+            filePath.slice(filePath.lastIndexOf('.'))
+        )
+        
+        if (!gen) throw new Error(`File type not supportted! ${filePath}`)
+        
+        this.strategy = new gen(fileHandler)
+    }
+
+    async sendData (container) {
+
+        const query = await this.strategy.execute()
+        const filename = `${uuidv4()}.sql`
+
+        const createfile = await container.exec({
+            Cmd: ["tee", filename],
+            Detach: false,
+            AttachStdout: false,
+            AttachStderr: false,
+            AttachStdin: true,
+        }) 
+        const create = await createfile.start({hijack: true, stdin: true})
+        container.modem.demuxStream(create, process.stdout, process.stderr)
+        create.write(query)
+
+
+        let dba_pwd = await container.inspect()
+        dba_pwd = dba_pwd.Config.Env
+            .find(variable => variable.match(/^DBA_PASSWORD=.*/i)); //get variable
+        dba_pwd = dba_pwd.slice(dba_pwd.indexOf('=') + 1) //get value
+               
+        const exec = await container.exec({
+            Cmd: ['/bin/bash', '-c', `isql 1111 dba ${dba_pwd} ${filename} && rm ${filename}`],
+            AttachStdout: true,
+            AttachStderr: true,
+            Detach: false,
+        })
+        const strm = await exec.start({hijack: true})
+        
+        container.modem.demuxStream(strm, process.stdout, process.stderr)
+
+        create.destroy('')
+        strm.destroy('')
+    }
+}
--- index.js
+++ index.js
@@ -1,20 +1,100 @@
+const fsp = require('fs/promises');
+
 const core = require('@actions/core');
-const { exec, execSync } = require('child_process');
+const Dockerode = require("dockerode");
 
-try {
-    const dba_password = core.getInput('dba_password');
-    const dav_password = core.getInput('dav_password');
-    const triples = core.getInput('triples');
+const Filler = require('./fillData');
 
-    core.setOutput(execSync('ls'))
-    core.setOutput(execSync('pwd'))
 
-    exec(`./entrypoint.sh ${dba_password} ${dav_password} ${triples}`, (error, stdout, stderr) => {
-        if (error) throw [ error, stdout, stderr ];
+// watches the container stdout and wait for the ready string to be printed
+function waitForVirtuosoInit(stream) {
+    return new Promise((resolve, reject) => {
+        stream.on('error', (data) => reject(data.toString()))
+        stream.on('data', (data) => {
+            console.log(data.toString())
+            if (data.toString().includes('Server online at 1111')) {
+                resolve();
+            }
+        }) 
+        stream.on('end', () => { //just in case
+            console.log("Ended!")          
+            resolve();
+        })
+    });
+  }
 
-        core.setOutput(stdout)
-    })
+async function bootstrap() {
+    "use strict"
 
-} catch (error) {
-    core.setFailed(`error: ${error}\nstdout: ${stdout}\nstderr: ${stderr}`);
+    try {
+        const dba_password = core.getInput('dba-password');
+        const dav_password = core.getInput('dav-password');
+        const triplesInput = core.getInput('triples');
+        const dbPort = core.getInput('publish-db-port');
+        const srvPort = core.getInput('publish-http-server-port')
+        
+        const docker = new Dockerode()
+        
+        await docker.pruneImages()
+        await docker.pruneContainers()
+        
+        const container = await docker.createContainer({
+            Env: [
+                `DBA_PASSWORD=${dba_password}`,
+                `DAV_PASSWORD=${dav_password}`,
+                
+                //virtuoso.ini
+                `VIRT_SPARQL_DEFAULTGRAPH=http://localhost/data`
+            ],
+            Image: "openlink/virtuoso-opensource-7",
+            Tty: false,
+            AttachStdout: true,
+            Detach: true,
+            OpenStdin: true,
+            Cmd: ['start'],
+            HostConfig: {
+                PortBindings: {
+                    "8890/tcp": [{
+                        HostPort: dbPort
+                    }],
+                    "1111/tcp": [{
+                        HostPort: srvPort
+                    }]
+                }
+            }
+        })
+        await container.start()
+
+        const stream = await container.attach({stream: true, stdout: true, stderr: true})
+        await waitForVirtuosoInit(stream)
+        stream.destroy('') //streams prevent program from stopping so we destroy them
+        
+
+        if (triplesInput && triplesInput.length !== 0) {
+            const files = triplesInput.split(' ');
+            const fillers = []
+
+            try {
+                for(let i = 0; i < files.length; ++i) {
+                    const fileHandler = await fsp.open(files[i], 'r')
+                    fillers.push(new Filler(files[i], fileHandler).sendData(container)) 
+                }
+                
+                console.log("Waiting for fillers to finish")
+                await Promise.all(fillers)
+                console.log("finished everything!!!");
+                return;
+            } catch(e) {
+                core.setFailed("Error when importing data from file, details: " + e);
+            }
+          
+        }
+        else console.log('No files provided')
+    
+    } catch (error) {
+        core.setFailed(error);
+    }
+
 }
+
+bootstrap()
